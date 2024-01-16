f6a373514349e93ae29f834cf823f17dce8f4dec
= <luistborges@outlook.com>
2024-01-16 15:26:47 +0000

    finished produtoController for now

--- leiloes/Controllers/ProdutoController.cs
+++ leiloes/Controllers/ProdutoController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
-using leiloes.Models; // Certifique-se de que este namespace está correto e contém a definição de Produto.
+using leiloes.Models; 
 using System.Threading.Tasks;
 
 namespace leiloes.Controllers
@@ -14,19 +14,23 @@ namespace leiloes.Controllers
             _context = context;
         }
 
+
+        // Mostra a página dos produtos
         public IActionResult Index()
         {
             var produtos = _context.Produtos.ToList();
             return View(produtos);
         }
 
-        // GET: Produto/Create
+        
+        
+        // CREATE -> Mostra a página de criação do produto
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Produto/Create
+        // CREATE -> Processa a criação do produto
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Nome,Descricao,Imagem,NumDonosAnt")] Produto produto)
@@ -35,99 +39,48 @@ namespace leiloes.Controllers
             {
                 _context.Produtos.Add(produto);
                 await _context.SaveChangesAsync();
-                return RedirectToAction("Index"); // Ajuste para onde você quer redirecionar após a criação
+                return RedirectToAction("Index"); // Ajustar
             }
 
             return View(produto);
         }
 
-        // GET: Produto/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
 
-            var produto = await _context.Produtos.FindAsync(id);
-            if (produto == null)
-            {
-                return NotFound();
-            }
-            return View(produto);
-        }
-
-        // POST: Produto/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("IdProduto,Nome,Descricao,Imagem,NumDonosAnt")] Produto produto)
-        {
-            if (id != produto.IdProduto)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(produto);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ProdutoExists(produto.IdProduto))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction("Index"); // Ajuste para onde você quer redirecionar após a edição
-            }
-
-            // Se o ModelState não for válido, ou o ID não corresponder, você deve retornar a View com o produto para tentar novamente.
-            return View(produto);
-        }
-
-        // GET: Produto/Delete/5
+        // DELETE -> Mostra a página de remoção do produto
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
-
             var produto = await _context.Produtos
                 .FirstOrDefaultAsync(m => m.IdProduto == id);
             if (produto == null)
             {
                 return NotFound();
             }
-
             return View(produto);
         }
 
-        // POST: Produto/Delete/5
+        // DELETE -> Processa a remoção do produto
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var produto = await _context.Produtos.FindAsync(id);
-            _context.Produtos.Remove(produto);
-            await _context.SaveChangesAsync();
-            return RedirectToAction("Index"); // Ajuste para onde você quer redirecionar após a exclusão
+            if (produto != null)
+            {
+                _context.Produtos.Remove(produto);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
         }
 
+        // Verifica se um produto existe -> Não usamos
         private bool ProdutoExists(int id)
         {
             return _context.Produtos.Any(e => e.IdProduto == id);
         }
-
-        // Não é necessário sobrescrever o método Dispose em controllers ASP.NET Core,
-        // pois o framework cuida disso através da injeção de dependência.
     }
 }
 
---
+++ leiloes/Views/Produto/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace leiloes.Views.Produto
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
