d9d8169158e83a092c96a34f5377899e2e7e6302
= <luistborges@outlook.com>
2024-01-28 15:14:06 +0000

    showing images now

---
+++ blazor/Models/ProdutoModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace blazor.Models
+{
+    public class ProdutoModel
+    {
+        public int IdProduto { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Imagem { get; set; }
+        public int NumDonosAnt { get; set; } = 0;
+    }
+}
